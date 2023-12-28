5e1cc6e2c794a7ac56530b8143a9f2c5bc10e8ac
rajnis2912 <rajnishsharma29120@gmail.com>
2023-12-28 22:50:02 +0530

    first

--- next.config.js
+++ next.config.js
@@ -3,5 +3,5 @@ const nextConfig = {
     output: 'export',
     env:{MONGODB_URL:"mongodb+srv://rajnishmig51:CKSRU2AztMpZoAbi@cluster0.ypskhez.mongodb.net/?retryWrites=true&w=majority"}
 }
-
+// nnn
 module.exports = nextConfig

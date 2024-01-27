cff419a11d925fd7d9927b502aee07f368e2ab11
rajnis2912 <rajnishsharma29120@gmail.com>
2024-01-27 22:40:19 +0530

    first

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,67 @@
 "use client"
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import { Formik, Field, Form, FormikHelpers } from 'formik';
+import { FaBars, FaTimes } from "react-icons/fa";
+
 import "../styles/style.css"
+import Link from 'next/link'
+import { useState } from 'react'
 const login = () => {
 
   interface Values {
     username: string;
     password: string;
   }
+  let router = useRouter()
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
 
   return (
     <>
+     
       <div className="login_box">
-        <h1 className="display-6 mb-3 text-white p-5">Login</h1>
+      <div className='nav-bar'>
+          <div>
+            <h1 className='font-bold'>RAJNISH SHARMA</h1>
+          </div>
+
+          <div className='menu'>
+            {/* <ul className="nav-menu gap-x-6"> */}
+            {/* <ul className="hidden md:flex gap-x-6 text-white"> */}
+            <ul className={click ? ("nav-menu active") : ("nav-menu")}>
+              <li>
+                <Link href="/skills">
+                  <p >SKILLS</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/projects">
+                  <p >PROJECT</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <p >ABOUT US</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contact">
+                  <p >CONTACT</p>
+                </Link>
+              </li>
+
+            </ul>
+            <div className="hamburger" onClick={handleClick}>
+
+              {click ? (<FaTimes size={20} style={{ color: "#000000" }} />) : (<FaBars size={20} style={{ color: "#000000" }} />)}
+
+            </div>
+
+          </div>
+
+        </div>
+        {/* <h1 className="display-6 mb-3 text-white p-5">Login</h1> */}
         <Formik
           initialValues={{
             username: '',

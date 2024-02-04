3c4f312568e8d327bd199152b713b900d7d7a255
rajnis2912 <rajnishsharma29120@gmail.com>
2024-02-04 12:41:39 +0530

    first

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: 'export',
-   images: { unoptimized: true },
-    env:{MONGODB_URL:"mongodb+srv://rajnishmig51:CKSRU2AztMpZoAbi@cluster0.ypskhez.mongodb.net/?retryWrites=true&w=majority"}
+    output: 'export',
+    // images: { unoptimized: true },
+    env: { MONGODB_URL: "mongodb+srv://rajnishmig51:CKSRU2AztMpZoAbi@cluster0.ypskhez.mongodb.net/?retryWrites=true&w=majority" }
 }
 
 module.exports = nextConfig
